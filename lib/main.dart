0e35acd64661a7dbcf0a92e41c0e33fbac7b490f
Blizzard03 <99153189+Blizzard03@users.noreply.github.com>
2023-11-20 22:22:34 +0700

    add CI-CD to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uas_ppb_4/screens/home_screen.dart';
+import 'package:ChefCompanion/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
