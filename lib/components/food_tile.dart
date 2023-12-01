15a801da68e12a7deaa316094c4dc4b6570a1634
mysthics <michaelmark991@gmail.com>
2023-12-01 14:05:54 +0700

    Updating pubspec.yaml, food_tile.dart, pricefood.dart and menu_screen.dart

--- lib/components/food_tile.dart
+++ lib/components/food_tile.dart
@@ -1,37 +1,69 @@
+import 'package:chef_companion/models/pricefood.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class FoodTile  extends StatefulWidget {
-  final Food food;
-  const FoodTile ({
+class FoodTile extends StatelessWidget {
+  final FoodPrice food;
+  const FoodTile({
     super.key,
     required this.food,
-    required this.onTap,
-    });
+  });
 
   @override
-  Widget build(BuildContext context)
-  return GestureDetector(
-    onTap: onTap,
-    child: Container(
+  Widget build(BuildContext context) {
+    return Container(
       decoration: BoxDecoration(
         color: Colors.grey[100],
-        borderRadius: BorderRadius.circular(20)
+        borderRadius: BorderRadius.circular(20),
       ),
       margin: const EdgeInsets.only(left: 25),
       padding: const EdgeInsets.all(25),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //image
-      )
-    ),
-  ),
-}
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          //image
+          Image.asset(
+            food.imagePath,
+            height: 140,
+          ),
+          //text
+          Text(
+            food.name,
+            style: GoogleFonts.dmSerifDisplay(fontSize: 20),
+          ),
+          //price + rating
+          SizedBox(
+            width: 160,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                //price
 
-class _FoodTile State extends State<FoodTile > {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+                Text(
+                  '\$' + food.price,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.grey[700]),
+                ),
+
+                //rating
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: Colors.yellow[800],
+                    ),
+                    Text(
+                      food.rating,
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
